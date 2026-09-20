fd9b27d5a04e09c09c95d6ba938f0639d7fe14fb
borjamateu <borjamateu.bm@gmail.com>
2026-09-21 00:12:30 +0200

    Sube versión de caché

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'entrenamiento-v2';
+const CACHE = 'entrenamiento-v3';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './comparativa.json'];
 
 self.addEventListener('install', (e) => {
