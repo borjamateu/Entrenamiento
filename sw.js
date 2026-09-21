ca7ea3161a87759fb18a1965325cdb3f1642b66b
borjamateu <borjamateu.bm@gmail.com>
2026-09-21 23:09:53 +0200

    Sube versión de caché a v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'entrenamiento-v3';
+const CACHE = 'entrenamiento-v4';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './comparativa.json'];
 
 self.addEventListener('install', (e) => {
